95c7441d7cd2aa459a9cab7ef6e5114e8bfe97e0
asbillt <davidasbill@asbillt.com>
2021-12-21 16:42:48 -0600

    Adds background animation styles. Creates Header component.

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+function Header() {
+  return (
+    <Container fluid>
+      <p>
+        Hey there! I'm David Asbill. <br />
+        I'm a Software Engineer
+      </p>
+      <button>View my work</button>
+    </Container>
+  );
+}
+
+export default Header;
