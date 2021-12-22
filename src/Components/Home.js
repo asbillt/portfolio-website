c7d947715424272b0f0461344b2e3a05e8fa14e8
asbillt <davidasbill@asbillt.com>
2021-12-22 17:11:01 -0600

    Adds bootstrap and icons import statements. Adds content to home component. Adds inline styles to home component.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,33 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import { CgArrowRight } from "react-icons/cg";
 
-function Header() {
+function Home() {
   return (
-    <Container fluid>
-      <p>
-        Hey there! I'm David Asbill. <br />
-        I'm a Software Engineer
-      </p>
-      <button>View my work</button>
+    <Container
+      fluid
+      className="vh-100 d-flex justify-content-center align-items-center"
+    >
+      <Row>
+        <h1 className="text-center text-white display-5 fw-normal px-0 pt-3 my-0">
+          Hello, I'm <span className="text-warning">David Asbill</span>. <br />
+          I'm a full-stack software engineer.
+        </h1>
+        <Col className="d-flex justify-content-center py-4">
+          <Button
+            variant="light"
+            className="d-flex fs-5 text-white bg-transparent"
+          >
+            <span className="px-2 py-1">Learn About Me</span>
+            <CgArrowRight className="align-self-center" />
+          </Button>
+        </Col>
+      </Row>
     </Container>
   );
 }
 
-export default Header;
+export default Home;
