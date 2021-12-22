37a2b5c317b10472086dea0488ac6f2f6ff1d6bc
asbillt <davidasbill@asbillt.com>
2021-12-21 20:40:09 -0600

    Adds react router routes. Creates about, projects, social components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import "./App.css";
 import Header from "./Components/Header";
+import About from "./Components/About";
+import Projects from "./Components/Projects";
+import Social from "./Components/Social";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Header />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+        <Route path="/social">
+          <Social />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Components/Projects.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Projects() {
+  return <p>TBD</p>;
+}
+
+export default Projects;
