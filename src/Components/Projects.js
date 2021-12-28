57f8dffa0f2799e8b1e13584423968bb921dc657
asbillt <davidasbill@asbillt.com>
2021-12-27 22:41:41 -0600

    Adds content and styling to projects component.

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,11 +1,96 @@
 import React from "react";
 import NavigationBar from "./NavigationBar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Projects() {
   return (
-    <div>
-      <NavigationBar />
-    </div>
+    <Container className="vh-100 text-white">
+      <Row className="">
+        <h1 className="text-center">Some apps I've built:</h1>
+      </Row>
+      <Row className="d-flex flex-column gy-5 p-5">
+        <Col className="bg-black bg-opacity-25">
+          <div className="d-flex flex-column">
+            <Col>
+              <h4>
+                Source Code:
+                <a
+                  href="https://github.com/asbillt/blog"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-decoration-none"
+                >
+                  Blog
+                </a>
+              </h4>
+              <h4>
+                Hosted App:
+                <a
+                  href="https://github.com/asbillt/blog"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-decoration-none"
+                >
+                  Blog
+                </a>
+              </h4>
+              <div className="text-center">
+                <h4>Blog</h4>
+                <p>A personal blog built in React.js.</p>
+              </div>
+            </Col>
+            <Col>
+              <h5>Built with:</h5>
+              <ul>
+                <li>React.js</li>
+                <li>JSON Server</li>
+              </ul>
+            </Col>
+          </div>
+        </Col>
+        <Col className="bg-black bg-opacity-25">
+          <div className="d-flex flex-column">
+            <Col>
+              <h4>
+                Source Code:
+                <a
+                  href="https://github.com/asbillt/blog"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-decoration-none"
+                >
+                  Blog
+                </a>
+              </h4>
+              <h4>
+                Hosted App:
+                <a
+                  href="https://github.com/asbillt/blog"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-decoration-none"
+                >
+                  Blog
+                </a>
+              </h4>
+              <div className="text-center">
+                <h4>Blog</h4>
+                <p>A personal blog built in React.js.</p>
+              </div>
+            </Col>
+            <Col>
+              <h5>Built with:</h5>
+              <ul>
+                <li>React.js</li>
+                <li>JSON Server</li>
+              </ul>
+            </Col>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
