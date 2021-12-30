39a3d500654b0e062e46616ebc65052a2a6eb250
asbillt <davidasbill@asbillt.com>
2021-12-30 17:40:26 -0600

    Adds slide in transition effect for home page content.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,6 +5,7 @@ import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import { useNavigate } from "react-router-dom";
 import { CgArrowRight } from "react-icons/cg";
+import { Slide } from "@mui/material";
 
 function Home() {
   const navigate = useNavigate();
@@ -14,19 +15,30 @@ function Home() {
       className="vh-100 d-flex justify-content-center align-items-center"
     >
       <Row>
-        <h1 className="text-center text-white display-5 fw-normal px-0 pt-3 my-0">
-          Hello, I'm <span className="text-warning">David Asbill</span>. <br />
-          I'm a full-stack software engineer.
-        </h1>
+        <Slide in={true} direction="right" easing="linear" timeout={750}>
+          <h1 className="text-center text-white display-5 fw-normal px-0 pt-3 my-0">
+            Hello, I'm <span className="text-warning">David Asbill</span>.{" "}
+            <br />
+            I'm a full-stack software engineer.
+          </h1>
+        </Slide>
         <Col className="d-flex justify-content-center py-4">
-          <Button
-            variant="warning"
-            className="homebtn border-1 d-flex fs-5 text-warning bg-transparent"
-            onClick={() => navigate("/about")}
+          <Slide
+            in={true}
+            direction="right"
+            easing="linear"
+            timeout={750}
+            style={{ transitionDelay: "750ms" }}
           >
-            <span className="px-2 py-1">Learn About Me</span>
-            <CgArrowRight className="align-self-center" />
-          </Button>
+            <Button
+              variant="warning"
+              className="homebtn border-1 d-flex fs-5 text-warning bg-transparent"
+              onClick={() => navigate("/about")}
+            >
+              <span className="px-2 py-1">Learn About Me</span>
+              <CgArrowRight className="align-self-center" />
+            </Button>
+          </Slide>
         </Col>
       </Row>
     </Container>
