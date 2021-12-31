c659ae53ae3c21a45944b895f6085999d6a77879
asbillt <davidasbill@asbillt.com>
2021-12-30 18:30:08 -0600

    Updates breakpoint collapse and justification for navigation bar.

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -6,38 +6,37 @@ import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 import { Link } from "react-router-dom";
 
 // Create function componenent, NavigationBar.
-// Navbar component with a navbar-expand property set to lg (992px) breakpoint for responsive collapsing.
-// NavbarBrand component for your company, product, or project name.
-// NavbarToggle component for a hamburger icon under 992 px breakpoint.
+// Navbar component with a navbar-expand property set to md (768px) breakpoint for responsive collapsing.
+// NavbarToggle component for a hamburger icon under 768 px breakpoint.
 // NavbarCollapse component for set of navigation links that are grouped and
-// hidden by a parent breakpoint of 992px.
+// hidden by a parent breakpoint of 768px.
 function NavigationBar() {
   return (
-    <Navbar expand="lg" className="px-5 py-3 bg-light" fixed="top">
+    <Navbar expand="md" className="px-5 py-3 bg-light" fixed="top">
       <NavbarToggle
         aria-controls="basic-navbar-nav"
         className=""
       ></NavbarToggle>
-      <NavbarCollapse id="basic-navbar-nav" className="justify-content-start">
-        <Nav className="px-lg-5 fs-5">
-          <Link to="/" className="px-lg-4 py-lg-1 py-1 text-decoration-none">
+      <NavbarCollapse id="basic-navbar-nav" className="justify-content-center">
+        <Nav className="px-md-5 fs-5">
+          <Link to="/" className="px-md-5 py-md-1 py-1 text-decoration-none">
             Home
           </Link>
           <Link
             to="/about"
-            className="px-lg-4 py-lg-1 py-1 text-decoration-none"
+            className="px-md-5 py-md-1 py-1 text-decoration-none"
           >
             About
           </Link>
           <Link
             to="/projects"
-            className="px-lg-4 py-lg-1 py-1 text-decoration-none"
+            className="px-md-5 py-md-1 py-1 text-decoration-none"
           >
             Projects
           </Link>
           <Link
             to="/social"
-            className="px-lg-4 py-lg-1 py-1 text-decoration-none"
+            className="px-md-5 py-md-1 py-1 text-decoration-none"
           >
             Social
           </Link>
