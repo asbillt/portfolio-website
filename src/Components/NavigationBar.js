af745750fd3992d0c888448e678b40ede9f5a221
asbillt <davidasbill@asbillt.com>
2021-12-30 21:23:46 -0600

    Adds social links to navigation bar.

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -4,6 +4,13 @@ import { Navbar, Nav } from "react-bootstrap";
 import NavbarToggle from "react-bootstrap/esm/NavbarToggle";
 import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 import { Link } from "react-router-dom";
+import {
+  GrLinkedin,
+  GrGithub,
+  GrStackOverflow,
+  GrYoutube,
+  GrTwitter,
+} from "react-icons/gr";
 
 // Create function componenent, NavigationBar.
 // Navbar component with a navbar-expand property set to md (768px) breakpoint for responsive collapsing.
@@ -17,7 +24,7 @@ function NavigationBar() {
         aria-controls="basic-navbar-nav"
         className=""
       ></NavbarToggle>
-      <NavbarCollapse id="basic-navbar-nav" className="justify-content-center">
+      <NavbarCollapse id="basic-navbar-nav" className="justify-content-start">
         <Nav className="px-md-5 fs-5">
           <Link to="/" className="px-md-5 py-md-1 py-1 text-decoration-none">
             Home
@@ -34,14 +41,33 @@ function NavigationBar() {
           >
             Projects
           </Link>
-          <Link
-            to="/social"
-            className="px-md-5 py-md-1 py-1 text-decoration-none"
-          >
-            Social
-          </Link>
         </Nav>
       </NavbarCollapse>
+      <div className="d-flex justify-content-center ">
+        <a
+          href="https://www.linkedin.com/in/davidasbill/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <GrLinkedin size={30} className="me-3 me-lg-5" />
+        </a>
+        <a href="https://github.com/asbillt" target="_blank" rel="noreferrer">
+          <GrGithub size={30} className="me-3 me-lg-5" />
+        </a>
+        <a
+          href="https://stackoverflow.com/users/16709095/david-asbill"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <GrStackOverflow size={30} className="me-3 me-lg-5" />
+        </a>
+        <a href="https://www.youtube.com/" target="_blank" rel="noreferrer">
+          <GrYoutube size={30} className="me-3 me-lg-5" />
+        </a>
+        <a href="https://twitter.com/asbillt1" target="_blank" rel="noreferrer">
+          <GrTwitter size={30} className="me-lg-5" />
+        </a>
+      </div>
     </Navbar>
   );
 }
