52801fc595f8eb17c76bb7f0acb8d2421ac5a86d
asbillt <davidasbill@asbillt.com>
2022-01-05 21:28:17 -0600

    Removes home component styles from app.css. Updates home component with mui grid. Updates home button styling.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
+import { Grid } from "@mui/material";
+import { Container } from "@mui/material";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
@@ -10,38 +11,40 @@ import { Slide } from "@mui/material";
 function Home() {
   const navigate = useNavigate();
   return (
-    <Container
-      fluid
-      className="vh-100 d-flex justify-content-center align-items-center"
-    >
-      <Row>
-        <Slide in={true} direction="right" easing="ease-out" timeout={1000}>
-          <h1 className="text-center text-white display-5 fw-normal px-0 pt-3 my-0">
-            Hello, I'm <span className="text-warning">David Asbill</span>.{" "}
-            <br />
-            I'm a full-stack software engineer.
-          </h1>
-        </Slide>
-        <Col className="d-flex justify-content-center py-4">
-          <Slide
-            in={true}
-            direction="right"
-            easing="ease-out"
-            timeout={1000}
-            style={{ transitionDelay: "750ms" }}
-          >
-            <Button
-              variant="warning"
-              className="homebtn border-1 d-flex fs-5 text-warning bg-transparent"
-              onClick={() => navigate("/about")}
-            >
-              <span className="px-2 py-1">Learn About Me</span>
-              <CgArrowRight className="align-self-center" />
-            </Button>
+    <Grid>
+      <Container
+        fluid
+        className="vh-100 d-flex justify-content-center align-items-center"
+      >
+        <Row>
+          <Slide in={true} direction="right" easing="ease-out" timeout={1000}>
+            <h1 className="text-center text-white display-5 fw-normal px-0 pt-3 my-0">
+              Hello, I'm <span className="text-warning">David Asbill</span>.{" "}
+              <br />
+              I'm a front end software engineer.
+            </h1>
           </Slide>
-        </Col>
-      </Row>
-    </Container>
+          <Col className="d-flex justify-content-center py-4">
+            <Slide
+              in={true}
+              direction="right"
+              easing="ease-out"
+              timeout={1000}
+              style={{ transitionDelay: "750ms" }}
+            >
+              <Button
+                variant="outline-warning"
+                className="border-1 d-flex fs-5"
+                onClick={() => navigate("/about")}
+              >
+                <span className="px-2 py-1">Learn About Me</span>
+                <CgArrowRight className="align-self-center" />
+              </Button>
+            </Slide>
+          </Col>
+        </Row>
+      </Container>
+    </Grid>
   );
 }
 
