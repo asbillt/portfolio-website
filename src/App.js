e19c4b20cda8b7a8f6c3600c70f5d4dd8248ed57
asbillt <davidasbill@asbillt.com>
2022-01-12 13:29:26 -0600

    Creates 404 page not found component. Adds corresponding route.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // Import statements for App.css local styles,
-// Home, About and Projects components,
+// Home, About, Projects and NotFound components,
 // and the Router, Routes and Route components for navigation.
 import "./App.css";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Projects from "./Components/Projects";
+import NotFound from "./Components/NotFound";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Return a set of Routes for app url navigation/component rendering.
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
---
+++ src/Components/NotFound.js
@@ -0,0 +1,26 @@
+// Import react library and react router link component.
+import React from "react";
+import { Link } from "react-router-dom";
+
+function NotFound() {
+  return (
+    // Return the 404 div component below.
+    <div className="vh-100 text-white d-flex justify-content-center align-items-center flex-column fs-1">
+      <h1>404</h1>
+      <p className="text-center">
+        Page Not Found <br />
+        Go Back To{" "}
+        <Link
+          // Link routes to the home page.
+          to="/"
+          className="link linkhover"
+        >
+          Home
+        </Link>
+      </p>
+    </div>
+  );
+}
+
+// Export the default component NotFound.
+export default NotFound;
