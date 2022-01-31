77e63a4fcaf437f0e16506987d063c3e26e164fa
asbillt <davidasbill@asbillt.com>
2022-01-31 16:52:13 -0600

    Adds additional projects.

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -44,40 +44,40 @@ function Projects() {
               lg={6}
               xxl={4}
             >
-              <div className="height backgroundColorBlog backgroundBorder p-3 d-flex flex-column">
+              <div className="height backgroundColorDwellings backgroundBorder p-3 d-flex flex-column">
                 <Col>
                   <h4>
                     Source Code:
                     <a
-                      href="https://github.com/asbillt/blog"
+                      href="https://github.com/asbillt/dwellings"
                       target="_blank"
                       rel="noopener noreferrer"
                       className="text-decoration-none text-warning ps-2"
                     >
-                      Blog
+                      Dwellings
                     </a>
                   </h4>
                   <h4>
                     Hosted App:
-                    <a
-                      href="https://github.com/asbillt/blog"
+                    {/* <a
+                      href="https://github.com/asbillt/dwellings"
                       target="_blank"
                       rel="noopener noreferrer"
                       className="text-decoration-none text-warning ps-2"
                     >
-                      Blog
-                    </a>
+                      Dwellings
+                    </a> */}
                   </h4>
                   <div className="text-center p-4">
-                    <h4>Blog</h4>
-                    <p>A personal blog built in React.js.</p>
+                    <h4>Dwellings (In Progress)</h4>
+                    <p>A real estate app built in Next.js.</p>
                   </div>
                 </Col>
                 <Col>
                   <h5>Built with:</h5>
                   <ul>
-                    <li>React.js</li>
-                    <li>JSON Server</li>
+                    <li>Next.js</li>
+                    <li>Chakra UI</li>
                   </ul>
                 </Col>
               </div>
@@ -85,10 +85,62 @@ function Projects() {
           </Fade>
           <Fade
             in={true}
-            direction="right"
             easing="ease-out"
             timeout={500}
             style={{ transitionDelay: 500 }}
+          >
+            <Col
+              // Col spans 12 columns from xs through md breakpoints.
+              // Col spans 6 columns from lg through xl breakpoints.
+              // Col spans 4 columns xxl and above breakpoint.
+              xs={12}
+              lg={6}
+              xxl={4}
+            >
+              <div className="height backgroundColorEcosystems backgroundBorder p-3 d-flex flex-column">
+                <Col>
+                  <h4>
+                    Source Code:
+                    <a
+                      href="https://github.com/asbillt/react-ecosystems"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-decoration-none text-warning ps-2"
+                    >
+                      React Ecosystems
+                    </a>
+                  </h4>
+                  <h4>
+                    Hosted App:
+                    {/* <a
+                      href="https://github.com/asbillt/react-ecosystems"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-decoration-none text-warning ps-2"
+                    >
+                      React Ecosystems
+                    </a> */}
+                  </h4>
+                  <div className="text-center p-4">
+                    <h4>Todo List</h4>
+                    <p>A todo list app built in React.js.</p>
+                  </div>
+                </Col>
+                <Col>
+                  <h5>Built with:</h5>
+                  <ul>
+                    <li>React.js</li>
+                    <li>Redux</li>
+                  </ul>
+                </Col>
+              </div>
+            </Col>
+          </Fade>
+          <Fade
+            in={true}
+            easing="ease-out"
+            timeout={500}
+            style={{ transitionDelay: 750 }}
           >
             <Col
               // Col spans 12 columns from xs through md breakpoints.
@@ -114,7 +166,7 @@ function Projects() {
                   <h4>
                     Hosted App:
                     <a
-                      href="https://github.com/asbillt/portfolio-website"
+                      href="https://asbillt.github.io/portfolio-website/"
                       target="_blank"
                       rel="noopener noreferrer"
                       className="text-decoration-none text-warning ps-2"
@@ -124,7 +176,7 @@ function Projects() {
                   </h4>
                   <div className="text-center p-4">
                     <h4>Portfolio Website</h4>
-                    <p>A personal portofolio website built in React.js.</p>
+                    <p>A personal portfolio website built in React.js.</p>
                   </div>
                 </Col>
                 <Col>
@@ -133,60 +185,6 @@ function Projects() {
                     <li>React.js</li>
                     <li>Material UI</li>
                     <li>Bootstrap</li>
-                  </ul>
-                </Col>
-              </div>
-            </Col>
-          </Fade>
-          <Fade
-            in={true}
-            direction="right"
-            easing="ease-out"
-            timeout={500}
-            style={{ transitionDelay: 750 }}
-          >
-            <Col
-              // Col spans 12 columns from xs through md breakpoints.
-              // Col spans 6 columns from lg through xl breakpoints.
-              // Col spans 4 columns xxl and above breakpoint.
-              xs={12}
-              lg={6}
-              xxl={4}
-            >
-              <div className="height backgroundColorAsbillt backgroundBorder p-3 d-flex flex-column">
-                <Col>
-                  <h4>
-                    Source Code:
-                    <a
-                      href="https://github.com/asbillt/asbillt-website"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="text-decoration-none text-warning ps-2"
-                    >
-                      Asbillt Website V1
-                    </a>
-                  </h4>
-                  <h4>
-                    Hosted App:
-                    <a
-                      href="https://asbillt.com/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="text-decoration-none text-warning ps-2"
-                    >
-                      Asbillt Website V1
-                    </a>
-                  </h4>
-                  <div className="text-center p-4">
-                    <h4>Portfolio Website</h4>
-                    <p>A personal portofolio website built in React.js.</p>
-                  </div>
-                </Col>
-                <Col>
-                  <h5>Built with:</h5>
-                  <ul>
-                    <li>HTML5</li>
-                    <li>CSS3</li>
                   </ul>
                 </Col>
               </div>
@@ -207,39 +205,40 @@ function Projects() {
               lg={6}
               xxl={4}
             >
-              <div className="height backgroundColorTea backgroundBorder p-3 d-flex flex-column">
+              <div className="height backgroundColorAsbillt2 backgroundBorder p-3 d-flex flex-column">
                 <Col>
                   <h4>
                     Source Code:
                     <a
-                      href="https://github.com/asbillt/asbillt-website"
+                      href="https://github.com/asbillt/asbillt-website-v2"
                       target="_blank"
                       rel="noopener noreferrer"
                       className="text-decoration-none text-warning ps-2"
                     >
-                      Tea Party Website
+                      Asbillt Website V2
                     </a>
                   </h4>
                   <h4>
                     Hosted App:
-                    <a
-                      href="https://asbillt.com/"
+                    {/* <a
+                      href="https://github.com/asbillt/asbillt-website-v2"
                       target="_blank"
                       rel="noopener noreferrer"
                       className="text-decoration-none text-warning ps-2"
                     >
-                      Tea Party Website
-                    </a>
+                      Asbillt Website V2
+                    </a> */}
                   </h4>
                   <div className="text-center p-4">
-                    <h4>Tea Party Website</h4>
-                    <p>A tea party website built in React.js.</p>
+                    <h4>Asbillt.com (In Progress)</h4>
+                    <p>Updated responsive version of Asbillt.com.</p>
                   </div>
                 </Col>
                 <Col>
                   <h5>Built with:</h5>
                   <ul>
                     <li>React.js</li>
+                    <li>Material UI</li>
                     <li>Bootstrap</li>
                   </ul>
                 </Col>
@@ -252,6 +251,116 @@ function Projects() {
             easing="ease-out"
             timeout={500}
             style={{ transitionDelay: 1250 }}
+          >
+            <Col
+              // Col spans 12 columns from xs through md breakpoints.
+              // Col spans 6 columns from lg through xl breakpoints.
+              // Col spans 4 columns xxl and above breakpoint.
+              xs={12}
+              lg={6}
+              xxl={4}
+            >
+              <div className="height backgroundColorAsbillt1 backgroundBorder p-3 d-flex flex-column">
+                <Col>
+                  <h4>
+                    Source Code:
+                    <a
+                      href="https://github.com/asbillt/Asbillt-Website-V1"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-decoration-none text-warning ps-2"
+                    >
+                      Asbillt Website V1
+                    </a>
+                  </h4>
+                  <h4>
+                    Hosted App:
+                    <a
+                      href="https://asbillt.com/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-decoration-none text-warning ps-2"
+                    >
+                      Asbillt Website V1
+                    </a>
+                  </h4>
+                  <div className="text-center p-4">
+                    <h4>Asbillt.com</h4>
+                    <p>Business website that communicates services.</p>
+                  </div>
+                </Col>
+                <Col>
+                  <h5>Built with:</h5>
+                  <ul>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                  </ul>
+                </Col>
+              </div>
+            </Col>
+          </Fade>
+          <Fade
+            in={true}
+            direction="right"
+            easing="ease-out"
+            timeout={500}
+            style={{ transitionDelay: 1500 }}
+          >
+            <Col
+              // Col spans 12 columns from xs through md breakpoints.
+              // Col spans 6 columns from lg through xl breakpoints.
+              // Col spans 4 columns xxl and above breakpoint.
+              xs={12}
+              lg={6}
+              xxl={4}
+            >
+              <div className="height backgroundColorBlog backgroundBorder p-3 d-flex flex-column">
+                <Col>
+                  <h4>
+                    Source Code:
+                    <a
+                      href="https://github.com/asbillt/blog"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-decoration-none text-warning ps-2"
+                    >
+                      Blog
+                    </a>
+                  </h4>
+                  <h4>
+                    Hosted App:
+                    {/* <a
+                      href="https://github.com/asbillt/blog"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-decoration-none text-warning ps-2"
+
+                    >
+                      Blog
+                    </a> */}
+                  </h4>
+                  <div className="text-center p-4">
+                    <h4>Blog (In Progress)</h4>
+                    <p>A personal blog built in React.js.</p>
+                  </div>
+                </Col>
+                <Col>
+                  <h5>Built with:</h5>
+                  <ul>
+                    <li>React.js</li>
+                    <li>Material UI</li>
+                    <li>Bootstrap</li>
+                  </ul>
+                </Col>
+              </div>
+            </Col>
+          </Fade>
+          <Fade
+            in={true}
+            direction="right"
+            easing="ease-out"
+            timeout={500}
+            style={{ transitionDelay: 1750 }}
           >
             <Col
               // Col spans 12 columns from xs through md breakpoints.
@@ -287,15 +396,15 @@ function Projects() {
                   </h4>
                   <div className="text-center p-4">
                     <h4>Color Flipper</h4>
-                    <p>Color Flipper</p>
+                    <p>A website where users can set website colors.</p>
                   </div>
                 </Col>
                 <Col>
                   <h5>Built with:</h5>
                   <ul>
                     <li>JavaScript (ES6)</li>
-                    <li>CSS3</li>
                     <li>HTML5</li>
+                    <li>CSS3</li>
                   </ul>
                 </Col>
               </div>
